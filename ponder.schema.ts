94a25a848766be4459b4c5aabbc95b10b6dfc323
Damian <damianmarti@gmail.com>
2024-04-22 14:55:39 -0300

    Add Container entity

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,8 +1,9 @@
 import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
-  Example: p.createTable({
+  Container: p.createTable({
     id: p.string(),
-    name: p.string().optional(),
+    owner: p.string(),
+    contract: p.string(),
   }),
 }));
--- src/BasedKudzuContainerForSaleFactory.ts
+++ src/BasedKudzuContainerForSaleFactory.ts
@@ -3,6 +3,15 @@ import { ponder } from "@/generated";
 ponder.on(
   "BasedKudzuContainerForSaleFactory:Created",
   async ({ event, context }) => {
+    const { Container } = context.db;
     console.log(event.args);
+
+    await Container.create({
+      id: event.args.contractAddress,
+      data: {
+        owner: event.args.owner,
+        contract: event.args.contractAddress,
+      },
+    });
   },
 );
